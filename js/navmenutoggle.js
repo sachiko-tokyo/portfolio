7bb1f225b3339d01ce6469e5f686b87843f915f9
sachiko0811 <sachikonovel@yahoo.co.jp>
2023-03-09 02:32:32 +0900

    bug format

--- js/navmenutoggle.js
+++ js/navmenutoggle.js
@@ -1,25 +1,16 @@
 $(function() {
+  $(function() {
+    $('.btn-trigger').on('click', function() {
+      $(this).toggleClass('active');
 
-    $(function(){
-        $('.btn-trigger').on('click', function() {
-          // $(this).toggleClass('active');
-
-          // remove "fadeInDown" class before jumping to another section
-          if ($(this).hasClass('active')) {
-              let nodeList = document.querySelectorAll('.fadeInDown');
-              let items = Array.from(nodeList);
-              items.map(i => {
-                  i.classList.remove('fadeInDown')
-              })
-              $(this).toggleClass('active');
-              $('body').css('overflow', 'hidden');
-          }
-
-          $('.nav-toggle, .nav-second-nav').toggleClass('show');
-          document.body.classList.toggle('popup-stop-scroll')
-          return false;
+      // remove "fadeInDown" class before jumping to another section
+      if ($(this).hasClass('active')) {
+        $('.nav-toggle, .nav-second-nav').toggleClass('show');
+        $(".nav-menu-li a").on("click", function() {
+          $(".nav-second-nav").removeClass("show");
+          $(".btn-trigger").removeClass("active")
         });
-      });
-
+      }
+    });
+  });
 })
-
